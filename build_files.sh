8a7aefe3d94ca6ae862db0ae9bb7ad7bfe9689a1
Nazir-Iqbal <naziriqbal01102003@gmail.com>
2026-04-18 11:25:34 +0000

    resolving the static file deployment issue

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Building static files..."
+set -e
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
